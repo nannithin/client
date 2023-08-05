a3ae4fd59fb3c97bf1208784579451e43da4570b
nannithin <nithinyalakala@gmail.com>
2023-08-06 00:16:12 +0530

    updated

---
+++ src/compoents/todo.js
@@ -0,0 +1,16 @@
+
+
+const Todo = () => {
+    return (
+        <div>
+          <h1>Todo app</h1>
+          <div>
+            <form>
+              <input></input>
+            </form>
+          </div>
+        </div>
+    )
+}
+
+export default Todo;
